f28b3631a05330ece4d26c1ec98e337ef336581d
dli459 <55778381+dli459@users.noreply.github.com>
2022-08-07 21:59:06 +1200

    Update_version3
    Update_version3

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -17,17 +17,18 @@ export default function BasicGrid(props: any) {
 
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid
-                marginTop="100px"
+        <Box sx={{ flexGrow: 1, padding:10 }}>
+            <Grid 
+                marginTop="50px"
                 container
                 direction="row"
                 justifyContent="center"
                 alignItems="center"
+
             >
                 {props.recipes.map((recipe:any )=> {
                     return  (  
-                    <Grid marginRight="50px" marginBottom="50px">
+                    <Grid marginRight="50px" marginBottom="50px" >
                     <RecipeReviewCard recipe={recipe} ></RecipeReviewCard>
                     </Grid>)           
                 })}
--- src/components/FoodApp.tsx
+++ src/components/FoodApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TextField } from "@mui/material";
-import { Button } from "@mui/material";
+import LoadingButton from '@mui/lab/LoadingButton';
 import getRecipe from "../api/getRecipe"
 import BasicGrid from './CardGrid'
 
@@ -9,13 +9,16 @@ const FoodApp = (props: any) => {
 
     const [recipes, setRecipes] = React.useState([])
     const [input, setInput] = React.useState("");
+    const [loading,setLoading] = React.useState(false);
 
     const handelInputChange = (event: any) => {
         setInput(event.target.value);
     }
     const handleSearchRecipe = async () => {
+        setLoading(true)
         const res = await getRecipe(input);
         setRecipes(res.results)
+        setLoading(false)
     }
 
 
@@ -23,10 +26,10 @@ const FoodApp = (props: any) => {
     return (
         <div>
         <div style={{ marginTop: '20px' }} >
-            <TextField label="Search The Restaurant" variant="filled" color="secondary" style={{ width: "400px" }} onChange={e => handelInputChange(e)} />
-            <Button variant="outlined" color="secondary" style={{ height: "56px", marginLeft: '10px' }} onClick={handleSearchRecipe}>
+            <TextField label="Search The Recipe" variant="filled" color="secondary" style={{ width: "400px" }} onChange={e => handelInputChange(e)} />
+            <LoadingButton loading={loading} variant="outlined" color="secondary" style={{ height: "56px", marginLeft: '10px' }} onClick={handleSearchRecipe}>
                 Search
-            </Button>
+            </LoadingButton>
         </div>
         <div>
             
