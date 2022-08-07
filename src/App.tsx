bb150f4f84ae151e3e0600349d49c8b3a1a34181
dli459 <55778381+dli459@users.noreply.github.com>
2022-08-07 17:24:47 +1200

    UI_complete
    complete UI part

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import FoodApp from './components/FoodApp'
+import "./App.css"
+import ButtonAppBar from './components/AppBar'
+import RecipeReviewCard from './components/RestaurantCard'
+import BasicGrid from './components/CardGrid'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonAppBar></ButtonAppBar>
+       <FoodApp></FoodApp>
+       <BasicGrid></BasicGrid>
     </div>
   );
 }
---
+++ src/components/FoodApp.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+
+const FoodApp = (props: any) => {
+
+    return (
+        <div>
+            <TextField label="Search the restaurant" variant="filled" color="secondary" />
+        </div>
+
+    )
+
+}
+
+
+export default FoodApp;
+
